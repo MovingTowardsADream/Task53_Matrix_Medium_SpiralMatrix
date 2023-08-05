896a718aadb0fed24875cc2ff9ffd4971b08fd12
Matvey <zxdfcvas4554@mail.ru>
2023-08-05 14:06:22 +0300

    Edit solution

--- Task53_Matrix_Medium_SpiralMatrix/Task53_Matrix_Medium_SpiralMatrix.cpp
+++ Task53_Matrix_Medium_SpiralMatrix/Task53_Matrix_Medium_SpiralMatrix.cpp
@@ -7,24 +7,21 @@ public:
         int n = matrix.size() - 1, m = matrix[0].size() - 1;
         int index = std::min(n, m) / 2;
         std::vector<int> result;
-        if (m == 0) {
-            for (int i = 0;i <= n;i++) {
-                result.push_back(matrix[i][0]);
-            }
-            return result;
-        }
         for (int i = 0;i <= index;i++) {
             for (int j = i;j <= m - i;j++) {
                 result.push_back(matrix[i][j]);
             }
             for (int j = i + 1;j <= n - i - 1;j++) {
-                result.push_back(matrix[j][m-i]);
+                result.push_back(matrix[j][m - i]);
             }
-            if (n-i == i) {
+            if (n - i == i) {
                 break;
             }
             for (int j = m - i;j >= i;j--) {
-                result.push_back(matrix[n-i][j]);
+                result.push_back(matrix[n - i][j]);
+            }
+            if (m - i == i) {
+                break;
             }
             for (int j = n - i - 1;j >= i + 1;j--) {
                 result.push_back(matrix[j][i]);
